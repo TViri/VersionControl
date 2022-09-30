eda94e15a0635a2b0bbb24e4ec8c426271ce0cd5
TViri <virag.tibenszki@stud.uni-corvinus.hu>
2022-09-30 22:37:37 +0200

    File write

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -22,6 +22,7 @@ namespace UserMaintenance
             label1.Text = Resource1.LastName; 
             label2.Text = Resource1.FirstName; 
             button1.Text = Resource1.Add;
+            button2.Text = Resource1.FileWrite;
 
             listBox1.DataSource = users;
             listBox1.ValueMember = "ID";
