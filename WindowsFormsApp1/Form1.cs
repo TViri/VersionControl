1a3eec7afd61eb97341c3222980ba8a7030ae178
TViri <virag.tibenszki@stud.uni-corvinus.hu>
2022-10-06 23:54:38 +0200

    Get Cell

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -103,5 +103,22 @@ namespace WindowsFormsApp1
         {
             flats = context.Flats.ToList();
         }
+
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
+        }
     }
 }
